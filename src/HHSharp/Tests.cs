157cc418447bc6ce954910440abc582c9694c852
Scott W Harden <swharden@gmail.com>
2019-10-20 15:20:23 -0400

    switch from NUnit to MSTest

--- src/HHSharp/Tests.cs
+++ src/HHSharp/Tests.cs
@@ -1,31 +1,39 @@
-﻿using NUnit.Framework;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace HHSharp
 {
-    [TestFixture]
+    [TestClass]
     public class Tests
     {
-        [Test]
+        private bool CloseEnough(double a, double b, double within = 0.00001)
+        {
+            return Math.Abs(a - b) < within;
+        }
+
+        [TestMethod]
         public void VerifySimulationValues()
         {
             // simulate constant excitatory current for 50ms
-            var hh = new HHModel();
+            var model = new HHModel();
             for (int i = 0; i < 5_000; i++)
-                hh.Iterate(stimulusCurrent: 20, deltaTms: 0.01);
+                model.Iterate(stimulusCurrent: 20, deltaTms: 0.01);
 
-            // ensure all values match what we expect
-            Assert.That(hh.Vm, Is.EqualTo(25.3868376949024).Within(0.0001));
-            Assert.That(hh.m.alpha, Is.EqualTo(1.04329640337898).Within(0.0001));
-            Assert.That(hh.m.beta, Is.EqualTo(0.951203951792703).Within(0.0001));
-            Assert.That(hh.n.alpha, Is.EqualTo(0.199386100874345).Within(0.0001));
-            Assert.That(hh.n.beta, Is.EqualTo(0.0904811444841807).Within(0.0001));
-            Assert.That(hh.h.alpha, Is.EqualTo(0.0192172096954907).Within(0.0001));
-            Assert.That(hh.h.beta, Is.EqualTo(0.397804384343861).Within(0.0001));
+            Console.WriteLine(model);
+
+            Assert.IsTrue(CloseEnough(model.Vm, 12.1344631950574));
+
+            Assert.IsTrue(CloseEnough(model.m.alpha, 0.508331886190113));
+            Assert.IsTrue(CloseEnough(model.m.beta, 1.97416521237869));
+            Assert.IsTrue(CloseEnough(model.m.activity, 0.598998710210472));
+
+            Assert.IsTrue(CloseEnough(model.h.alpha, 0.0370756858036909));
+            Assert.IsTrue(CloseEnough(model.h.beta, 0.150724202887592));
+            Assert.IsTrue(CloseEnough(model.h.activity, 0.0669057252257422));
+
+            Assert.IsTrue(CloseEnough(model.n.alpha, 0.114164951305969));
+            Assert.IsTrue(CloseEnough(model.n.beta, 0.106636971180963));
+            Assert.IsTrue(CloseEnough(model.n.activity, 0.73607630180196));
         }
     }
 }
